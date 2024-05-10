ac91841ac3d19475cd6b57fa99cd5288970ea7d3
Prajyot Tayde <prajyottayde@Prajyots-MacBook-Air.local>
2024-05-10 11:08:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
